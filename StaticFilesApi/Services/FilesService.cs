5d6da5f1ec21b890762aea23803a55143cd0a8ef
Kirill-basic <70901497+Kirill-basic@users.noreply.github.com>
2021-06-29 20:44:01 +0500

    Added files service

---
+++ StaticFilesApi/Services/FilesService.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using FileInfo = StaticFilesApi.Models.FileInfo;
+
+namespace StaticFilesApi.Services
+{
+    public class FilesService : IFilesService
+    {
+        public List<FileInfo> GetFileList()
+        {
+            var list = new List<FileInfo> { new FileInfo { Id = Guid.NewGuid().ToString(), Name = "test", Extension = ".png" } };
+
+            return list;
+        }
+
+
+        public FileInfo EditFileInfo(FileInfo fileInfo)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public FileInfo AddFile(FileStream file, FileInfo fileInfo)
+        {
+            return new FileInfo 
+            { 
+                Id = Guid.NewGuid().ToString(), 
+                Name = Path.GetFileName(file.Name), 
+                Extension = Path.GetExtension(file.Name) 
+            };
+        }
+
+
+        public FileInfo DeleteFile(string fileId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ StaticFilesApi/Services/IFilesService.cs
@@ -0,0 +1,18 @@
+﻿using StaticFilesApi.Models;
+using System.Collections.Generic;
+using System.IO;
+using FileInfo = StaticFilesApi.Models.FileInfo;
+
+namespace StaticFilesApi.Services
+{
+    public interface IFilesService
+    {
+        public List<FileInfo> GetFileList();
+
+        public FileInfo AddFile(FileStream file, FileInfo fileInfo);
+
+        public FileInfo EditFileInfo(FileInfo fileInfo);
+
+        public FileInfo DeleteFile(string fileId);
+    }
+}
