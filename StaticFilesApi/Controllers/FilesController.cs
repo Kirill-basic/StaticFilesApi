8dad9bc58f9564236c0e7740c42e5f7251d502c9
Kirill-basic <70901497+Kirill-basic@users.noreply.github.com>
2021-08-22 20:52:08 +0500

    added docs to the api

--- StaticFilesApi/Controllers/FilesController.cs
+++ StaticFilesApi/Controllers/FilesController.cs
@@ -2,7 +2,6 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Newtonsoft.Json;
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -20,6 +19,16 @@ namespace StaticFilesApi.Controllers
         }
 
 
+        /// <summary>
+        /// Get a list of FileModels
+        /// </summary>
+        /// <param></param>
+        /// <returns>
+        /// StatusCode with a list of FileModels
+        /// </returns>
+        /// <response code="200">If successful</response>
+        /// <response code="400">If error occurs</response>
+        /// <response code="404">If no files found</response>
         [HttpGet("[controller]")]
         public async Task<ActionResult<IEnumerable<FileModel>>> GetAsync()
         {
@@ -34,6 +43,16 @@ namespace StaticFilesApi.Controllers
         }
 
 
+        /// <summary>
+        /// Download file by id
+        /// </summary>
+        /// <param name="fileId">Id of a file</param>
+        /// <returns>
+        /// Stream of a file
+        /// </returns>
+        /// <response code="200">If successful</response>
+        /// <response code="400">If error occurs</response>
+        /// <response code="404">If file wasn't found</response>
         [HttpGet("[controller]/{fileId}")]
         public async Task<Stream> Get(string fileId)
         {
@@ -43,6 +62,16 @@ namespace StaticFilesApi.Controllers
         }
 
 
+        /// <summary>
+        /// Upload file to the api and save it
+        /// </summary>
+        /// <param name="file">Uploaded file</param>
+        /// <returns>
+        /// StatusCode with a FileModel of the saved file
+        /// </returns>
+        /// <response code="200">If successful</response>
+        /// <response code="400">If error occurs</response>
+        /// <response code="404">If no files found</response>
         [HttpPost("[controller]")]
         public async Task<ActionResult<FileModel>> PostAsync([FromForm] IFormFile file)
         {
@@ -57,7 +86,16 @@ namespace StaticFilesApi.Controllers
         }
 
 
-        //TODO:remove antiforgery later
+        /// <summary>
+        /// Update file name or file extension
+        /// </summary>
+        /// <param name="jsonModel">FileModel as JSON</param>
+        /// <returns>
+        /// statusCode with an updated FileModel
+        /// </returns>
+        /// <response code="200">If successful</response>
+        /// <response code="400">If error occurs</response>
+        /// <response code="404">If no files found</response>
         [HttpPut("[controller]")]
         public async Task<ActionResult<FileModel>> PutAsync([FromForm] string jsonModel)
         {
@@ -69,10 +107,20 @@ namespace StaticFilesApi.Controllers
         }
 
 
-        [HttpDelete("[controller]/{id}")]
-        public async Task<ActionResult<FileModel>> DeleteAsync([FromRoute] string id)
+        /// <summary>
+        /// Delete a file by its id
+        /// </summary>
+        /// <param name="fileId">Id of a file to delete</param>
+        /// <returns>
+        /// statusCode with a FileModel of a deleted file
+        /// </returns>
+        /// <response code="200">If successful</response>
+        /// <response code="400">If error occurs</response>
+        /// <response code="404">If no files found</response>
+        [HttpDelete("[controller]/{fileId}")]
+        public async Task<ActionResult<FileModel>> DeleteAsync([FromRoute] string fileId)
         {
-            var fileModel = await _filesService.DeleteAsync(id);
+            var fileModel = await _filesService.DeleteAsync(fileId);
 
             return fileModel;
         }
